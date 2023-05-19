bae8c951835190c5b0ae45d735ddba519731f2c5
AlpSantoGlobalMomentumLLC <128291039+AlpSantoGlobalMomentumLLC@users.noreply.github.com>
2023-05-19 22:46:57 +0200

    Update GermanRebootNotifier_GUI_PostponeOptions.ps1

--- GermanRebootNotifier_GUI_PostponeOptions.ps1
+++ GermanRebootNotifier_GUI_PostponeOptions.ps1
@@ -4,6 +4,7 @@
 # Axel Christian Lenz 
 # https://www.linkedin.com/in/axellenz/
 # https://github.com/AlpSantoGlobalMomentumLLC/PersonalPowerShellScripts/blob/main/GermanRebootNotifier_GUI_PostponeOptions.ps1
+# For NinjaOne you have to run it in the User Context.
 
 Function Create-GetSchedTime {
     Param( $SchedTime )
